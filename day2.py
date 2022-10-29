03e5dd4e485a96857a5f4eb01c190226dc567a57
Tommy Ngoy <tommy.ngoy@outlook.com>
2022-10-29 00:58:23 -0400

    day 2 part 2 code

--- day2.py
+++ day2.py
@@ -14,7 +14,26 @@ def pt1():
     
     return (abs(depth * horizontal))
 
+def pt2():
+    depth, horizontal = 0, 0 #y, x
+    aim = 0
+
+    with open("day2_input.txt", "r") as f:
+        data = [i for i in f.read().strip().split("\n")] #parse input
         
+        for line in data:
+            if "forward" in line:
+                forward = int(line.split(" ")[1])
+                horizontal += forward
+                depth += aim * forward
+            elif "up" in line:
+                aim -= int(line.split(" ")[1])
+            elif "down" in line:
+                aim += int(line.split(" ")[1]) 
     
+    return (abs(depth * horizontal))
+
+
 print(f'P1: {pt1()}')  
+print(f'P2: {pt2()}')  
     
